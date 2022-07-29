8a3e357427983b07936b736aa3ae41f131af7a6b
machinejun <akswns03@naver.com>
2022-07-29 16:40:02 +0900

    fltter 공부

---
+++ flutter_airbnb/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\utils\\flutter_windows_3.0.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\workspace_flutter\\flutter_airbnb" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\utils\\flutter_windows_3.0.4-stable\\flutter"
+  "PROJECT_DIR=C:\\workspace_flutter\\flutter_airbnb"
+  "FLUTTER_ROOT=C:\\utils\\flutter_windows_3.0.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\workspace_flutter\\flutter_airbnb\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\workspace_flutter\\flutter_airbnb"
+  "FLUTTER_TARGET=C:\\workspace_flutter\\flutter_airbnb\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\workspace_flutter\\flutter_airbnb\\.dart_tool\\package_config.json"
+)
---
+++ flutter_http_demo_1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\utils\\flutter_windows_3.0.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\workspace_flutter\\flutter_http_demo_1" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\utils\\flutter_windows_3.0.4-stable\\flutter"
+  "PROJECT_DIR=C:\\workspace_flutter\\flutter_http_demo_1"
+  "FLUTTER_ROOT=C:\\utils\\flutter_windows_3.0.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\workspace_flutter\\flutter_http_demo_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\workspace_flutter\\flutter_http_demo_1"
+  "FLUTTER_TARGET=C:\\workspace_flutter\\flutter_http_demo_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\workspace_flutter\\flutter_http_demo_1\\.dart_tool\\package_config.json"
+)
